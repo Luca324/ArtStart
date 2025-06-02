b20de6865ea26e0cecd05eb99b08e66e436e2f93
Im-Polina1029 <pollypolly03@mail.ru>
2025-06-03 00:16:03 +0300

    challenges ver1

--- ArtStart/Challenges.xaml.cs
+++ ArtStart/Challenges.xaml.cs
@@ -1,20 +1,99 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using ArtStart;
 
 namespace ArtStart
 {
-    /// <summary>
-    /// Логика взаимодействия для Challenges.xaml
-    /// </summary>
     public partial class Challenges : Window
     {
+        private List<Question> questions;
+        private int correctAnswers = 0;
+
         public Challenges()
         {
             InitializeComponent();
-
-            ColorMix.Click += Utils.Navigation_Click;
-            Paint.Click += Utils.Navigation_Click;
+            LoadQuestions();
+            GenerateUI();
         }
 
+        private void LoadQuestions()
+        {
+            // Пример 10 вопросов
+            questions = new List<Question>
+            {
+                new Question { Text = "Столица Франции?", Options = new List<string>{ "Париж", "Лондон", "Берлин" }, CorrectIndex = 0 },
+                new Question { Text = "2 + 2?", Options = new List<string>{ "3", "4", "5" }, CorrectIndex = 1 },
+                // Добавь ещё 8 вопросов
+                // ...
+            };
+        }
+
+        private void GenerateUI()
+        {
+            for (int i = 0; i < questions.Count; i++)
+            {
+                var question = questions[i];
+
+                var groupBox = new GroupBox
+                {
+                    Header = $"Вопрос {i + 1}: {question.Text}",
+                    Margin = new Thickness(5)
+                };
+
+                var stack = new StackPanel();
+
+                var radioGroup = new StackPanel();
+                var groupName = $"Question{i}";
+
+                for (int j = 0; j < question.Options.Count; j++)
+                {
+                    int questionIndex = i;
+                    int optionIndex = j;
+
+                    var radio = new RadioButton
+                    {
+                        Content = question.Options[j],
+                        GroupName = groupName,
+                        Margin = new Thickness(5)
+                    };
+
+                    radio.Checked += (s, e) =>
+                    {
+                        if (question.SelectedIndex != null)
+                            return; // Уже выбран, не менять
+
+                        question.SelectedIndex = optionIndex;
+                        bool isCorrect = optionIndex == question.CorrectIndex;
+
+                        if (isCorrect) correctAnswers++;
+
+                        // Подсветка
+                        foreach (RadioButton rb in radioGroup.Children)
+                        {
+                            int rbIndex = radioGroup.Children.IndexOf(rb);
+                            if (rbIndex == question.CorrectIndex)
+                                rb.Background = Brushes.LightGreen;
+                            else if (rb.IsChecked == true)
+                                rb.Background = Brushes.IndianRed;
+                        }
+
+                        // Показываем результат если последний вопрос
+                        if (questions.All(q => q.SelectedIndex != null))
+                        {
+                            ResultTextBlock.Text = $"Результат: {correctAnswers}/{questions.Count}";
+                        }
+                    };
+
+                    radioGroup.Children.Add(radio);
+                }
+
+                stack.Children.Add(radioGroup);
+                groupBox.Content = stack;
+                QuestionsPanel.Children.Add(groupBox);
+            }
+        }
     }
 }
---
+++ ArtStart/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtStart
+{
+    internal class Question
+    {
+        public string Text { get; set; }
+        public List<string> Options { get; set; }
+        public int CorrectIndex { get; set; }
+
+        // Для хранения выбранного пользователем ответа
+        public int? SelectedIndex { get; set; }
+    }
+}
