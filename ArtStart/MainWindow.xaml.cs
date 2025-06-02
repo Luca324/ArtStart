1640c09c32290cfaa9d7df3cbad4b56e9ce6f135
Luca324 <tanya.mosol@yandex.ru>
2025-06-02 20:45:40 +0300

    Update MainWindow.xaml.cs

--- ArtStart/MainWindow.xaml.cs
+++ ArtStart/MainWindow.xaml.cs
@@ -26,32 +26,5 @@ namespace ArtStart
         {
             this.Close();
         }
-
-        //private void Navigation_Click(object sender, RoutedEventArgs e)
-        //{
-        //    string senderName = ((FrameworkElement)sender).Name;
-
-        //    Console.WriteLine($"sender: {sender}\n senderName: {senderName}");
-
-        //    if (senderName == "Challenges")
-        //    {
-        //        var window = new Challenges();
-        //        window.Show();
-        //    }
-        //    else if (senderName == "Paint")
-        //    {
-        //        var window = new Paint();
-        //        window.Show();
-        //    }
-        //    else if (senderName == "ColorMix")
-        //    {
-        //        var window = new ColorMix();
-        //        window.Show();
-
-        //    }
-        //    this.Close();
-        //}
-
-
     }
 }
