88a14e6ae1a3bc19321dd20c1ed27e6658b49e3c
kiolymi <valaevai@yandex.ru>
2025-06-18 14:40:26 +0300

    пэинт доделала спрей

--- ArtStart/SprayTool.cs
+++ ArtStart/SprayTool.cs
@@ -45,20 +45,39 @@ namespace ArtStart
 
         private void Spray(Canvas canvas, Point center)
         {
-            for (int i = 0; i < sprayDensity; i++)
+            int densityMultiplier = 1;
+
+            if (sprayRadius < 10)
+            {
+                densityMultiplier = 1; // Оставляем как есть
+            }
+            else if (sprayRadius >= 10 && sprayRadius < 20)
+            {
+                densityMultiplier = 3; // Увеличиваем плотность в 3 раза
+            }
+            else if (sprayRadius >= 20 && sprayRadius <= 50)
+            {
+                densityMultiplier = 10; // Увеличиваем в 5 раз
+            }
+
+            int totalDots = sprayDensity * densityMultiplier;
+
+            for (int i = 0; i < totalDots; i++)
             {
                 double angle = random.NextDouble() * 2 * Math.PI;
                 double distance = random.NextDouble() * sprayRadius;
                 double x = center.X + distance * Math.Cos(angle);
                 double y = center.Y + distance * Math.Sin(angle);
+
                 Ellipse dot = new Ellipse
                 {
                     Width = 1,
                     Height = 1,
                     Fill = new SolidColorBrush(sprayColor)
                 };
-                Canvas.SetLeft(dot, x);
-                Canvas.SetTop(dot, y);
+
+                Canvas.SetLeft(dot, x - 0.5);
+                Canvas.SetTop(dot, y - 0.5);
                 canvas.Children.Add(dot);
             }
         }
