acef71d7ee8125821d7aedaac6be16a044526327
Im-Polina1029 <pollypolly03@mail.ru>
2025-06-20 11:12:38 +0300

    info2

---
+++ ArtStart/Info.xaml.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Windows;
+
+namespace ArtStart
+{
+    public partial class InfoWindow : Window
+    {
+        public InfoWindow()
+        {
+            InitializeComponent();
+
+            // Подписка на события кнопок
+            MainWindowBtn.Click += Utils.Navigation_Click;
+            PaintBtn.Click += Utils.Navigation_Click;
+            PaletteBtn.Click += Utils.Navigation_Click;
+            ChallengeBtn.Click += Utils.Navigation_Click;
+
+            // Закрытие текущего окна при переходе
+            MainWindowBtn.Click += CloseThis;
+            PaintBtn.Click += CloseThis;
+            PaletteBtn.Click += CloseThis;
+            ChallengeBtn.Click += CloseThis;
+        }
+
+        private void CloseThis(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
